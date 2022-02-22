4b187bd7192e3d0ef239e548fe669869c0059f0a
qcoliveros@gmail.com <SSuCM0b5Aj44>
2022-02-22 23:34:43 +0800

    Replaced the following db properties: 1. charset from utf8mb4 to utf8 2. collation from utf8mb4_unicode_ci to utf8_unicode_ci

--- app/Console/Commands/CreateDatabaseCommand.php
+++ app/Console/Commands/CreateDatabaseCommand.php
@@ -39,8 +39,8 @@ class CreateDatabaseCommand extends Command
     public function handle()
     {
         $schemaName = $this->argument('name') ?: config("database.connections.mysql.database");
-        $charset = config("database.connections.mysql.charset",'utf8mb4');
-        $collation = config("database.connections.mysql.collation",'utf8mb4_unicode_ci');
+        $charset = config("database.connections.mysql.charset",'utf8');
+        $collation = config("database.connections.mysql.collation",'utf8_unicode_ci');
 
         config(["database.connections.mysql.database" => null]);
 
