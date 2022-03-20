408b08f781f3a90f8a44ad4f48d15186f4640128
qcoliveros@gmail.com <SSuCM0b5Aj44>
2022-03-20 22:40:25 +0800

    Format the Join As field in register page.

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -6,7 +6,7 @@
             <form @submit.prevent="submit">
                 <div>
                     <jet-label for="role" value="Join As" />
-                    <Multiselect id="role" v-model="form.role" :options="['Gigger', 'Gig Host']" class="mt-1 mb-6 block w-full" required />
+                    <Multiselect id="role" v-model="form.role" :options="['Gigger', 'Gig Host']" required />
                 </div>
 
                 <div class="mt-4">
