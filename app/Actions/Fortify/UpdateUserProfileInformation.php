f6bc732dcf30183bf63ea665c6e71d293ac5d9ec
qcoliveros@gmail.com <SSuCM0b5Aj44>
2022-03-27 17:01:49 +0800

    1) Added the following fields for Profile section: a) Phone b) Messenger c) LinkedIn URL
    2) Retrieved the role list from database for register page.
    
    3) Corrected some layout issues.

--- app/Actions/Fortify/UpdateUserProfileInformation.php
+++ app/Actions/Fortify/UpdateUserProfileInformation.php
@@ -3,7 +3,6 @@
 namespace App\Actions\Fortify;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
 use Laravel\Fortify\Contracts\UpdatesUserProfileInformation;
